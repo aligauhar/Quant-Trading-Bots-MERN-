d69db9b2f61114ee0e8379d1818d76dd4d9958d6
Ali Gauhar <aligauharcb@gmail.com>
2024-03-12 18:13:22 +0500

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-const dburl = mongodb+srv://aligauhar:test123@cluster0.zugp10q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
+const dburl = "mongodb+srv://aligauhar:test123@cluster0.zugp10q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(dburl, {
   dbName: "schema",
