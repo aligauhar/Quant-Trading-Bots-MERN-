838c4b57e4c6f352434671215a703f3b84ac3c27
Ali Gauhar <aligauharcb@gmail.com>
2024-03-12 17:32:15 +0500

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
-export const dbConnection = () => {
 
-var dburl = "mongodb+srv://aligauhar:test123@cluster0.zugp10q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-var dbname = "schema";
+export const dbConnection = () => {
+  const dburl = "mongodb+srv://aligauhar:test123@cluster0.zugp10q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  const dbname = "schema";
+
   mongoose.connect(dburl, {
-    dbName:dbname,
+    dbName: dbname,
   })
-  .then(() => {
-      console.log("Connected to database!")
+    .then(() => {
+      console.log("Connected to the database!");
     })
-  .catch((err) => {
-      console.log(`Some error occured while connecing to database: ${err}`)
+    .catch((err) => {
+      console.log(`Some error occurred while connecting to the database: ${err}`);
     });
 };
