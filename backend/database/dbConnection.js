a787663baf71c59ee484deb2b2d665134e005bbf
Ali Gauhar <aligauharcb@gmail.com>
2024-03-12 21:27:20 +0500

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   const dburl = "mongodb+srv://aligauhar:test123@cluster0.zugp10q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   const dbname = "schema";
-
-
   
   mongoose.connect(dburl, {
     dbName: dbname,
